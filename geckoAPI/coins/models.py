97f8ee6e4c41fcf599f3486b65579eb67e85ea2e
kiririnshi <andres11soto253@outlook.com>
2026-04-18 13:14:12 -0400

    models ready

--- geckoAPI/coins/models.py
+++ geckoAPI/coins/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
-class Question(models.Model):
-    coin = models.CharField(max_length=3)
-    price = models.FloatField(default=0)
-    date = models.DateTimeField()
+class Moneda(models.Model): # CLP
+    nombre = models.CharField(max_length=10, unique=True)
+
+class Activo(models.Model): # BTC
+    nombre = models.CharField(max_length=10, unique=True)
+
+class Precio(models.Model): # CLP vs BTC
+    moneda = models.ForeignKey(Moneda, on_delete=models.CASCADE)
+    activo = models.ForeignKey(Activo, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=20, decimal_places=8) # Es mala practica usar float para datos financieros??? 
+    fecha_precio = models.DateTimeField() # La fecha en donde se reporto este precio.
+
+    class Meta:
+        unique_together = ('moneda', 'activo', 'fecha_precio')
+
+        
--- geckoAPI/date_conversor.py
+++ geckoAPI/date_conversor.py
@@ -1,3 +1,6 @@
 
-
-
+# Este script trata de resolver el problema de las fechas. Solo puedo consultar de 1 a 364 dias hacia atras desde el dia actual,
+# asi que hay que hacer lo siguiente:
+# - El usuario debe elegir una fecha desde ayer hasta 364 dias atras.
+# - Asi que el sistema debe guardar desde el principio todos los datos del pasado.
+# - 
